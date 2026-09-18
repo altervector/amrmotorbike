3693b5e9ae2032b066e386d223c6a7cd8bfa562b
genecarles <genecarles@gmail.com>
2026-09-18 04:31:31 +0200

    sit seg

--- config.js
+++ config.js
@@ -125,6 +125,4 @@ ON_SOM_TIT:        "Ven a vernos",
 
 // 6. SEGURETAT
 SITIOS_SEGUROS: ["alterwebstudio.com","pages.dev", "altervector.github.io", "amrmotorbike.es","amrmotorbike.com"],
-    SITIOS_SEGUROS: ["alterwebstudio.com", "pages.dev", "altervector.github.io", "localhost", "127.0.0.1"],
-
 };
