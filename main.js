24413a0217bfc30c499c8083684ca9dcce55bd99
genecarles <genecarles@gmail.com>
2026-09-24 20:32:35 +0200

    sfgj

--- main.js
+++ main.js
@@ -528,7 +528,7 @@ function carregarDadesModal() {
         if (containerMiniatures) containerMiniatures.style.display = 'none';
         if (blocSpecs) blocSpecs.style.display = 'none';
         const textWA = encodeURIComponent(`Hola, quiero vender mi moto.`);
-        document.getElementById('modal-btn-wa').href = `https://wa.me/34600000000?text=${textWA}`;
+        document.getElementById('modal-btn-wa').href = `https://wa.me/34669669877?text=${textWA}`;
     } else {
         // Restaurar vista normal per a les motos d'ocasió
         document.getElementById('modal-titol').innerText = `${moto.marca} ${moto.model}`;
@@ -550,7 +550,7 @@ function carregarDadesModal() {
         } else {
             textWA = encodeURIComponent(`Hola, estic interessat en la ${moto.marca} ${moto.model} (${moto.preu}€).`);
         }
-        document.getElementById('modal-btn-wa').href = `https://wa.me/34600000000?text=${textWA}`;
+        document.getElementById('modal-btn-wa').href = `https://wa.me/34669669877?text=${textWA}`;
     }
     
     // Imatge principal
